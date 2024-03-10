cd1801f472b99fc368d9760eefed07b9ece01a0e
Thuy Kieu <thuykieu130303@gmail.com>
2024-03-10 15:22:46 +0700

    Jdepend

---
+++ LeThiThuyKieu_Jdepend/LeThiThuyKieu_Jdepend/src/main/java/test/Main.java
@@ -0,0 +1,22 @@
+package test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jdepend.xmlui.JDepend;
+
+/**
+ * @author: Le Thi Thuy Kieu
+ * @date: Mar 7, 2024
+ * @description:
+ * 
+ **/
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		JDepend depend = new JDepend(new PrintWriter("report/report.xml"));
+		depend.addDirectory("D:\\KT&TKPM\\ThucHanh\\LeThiThuyKieu_Jdepend\\Library-Assistant-master");
+		depend.analyze();
+		System.out.println("DONE");
+	}
+}
