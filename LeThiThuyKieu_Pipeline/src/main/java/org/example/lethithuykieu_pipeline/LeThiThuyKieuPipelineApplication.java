afbe90ff396a683f807b313b3003006287a1eebd
Tika13lee <thuykieu130303@gmail.com>
2024-05-11 09:04:42 +0700

    pipeline

---
+++ LeThiThuyKieu_Pipeline/src/main/java/org/example/lethithuykieu_pipeline/LeThiThuyKieuPipelineApplication.java
@@ -0,0 +1,13 @@
+package org.example.lethithuykieu_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeThiThuyKieuPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeThiThuyKieuPipelineApplication.class, args);
+	}
+
+}
---
+++ LeThiThuyKieu_Pipeline/src/test/java/org/example/lethithuykieu_pipeline/LeThiThuyKieuPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lethithuykieu_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeThiThuyKieuPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
